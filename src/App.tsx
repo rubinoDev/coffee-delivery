b349cddfa3ed617bd382caeb463fb234610d2ec2
Victor Rubino <rubinodev@gmail.com>
2022-09-02 18:54:52 -0300

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { DefaultTheme } from './styles/themes/default'
 
 export function App() {
 
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={DefaultTheme}>
+      <h1>Hello world</h1>
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 62.5%;
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body{
+    background-color: ${props => props.theme.background};
+    color: ${props => props.theme.baseText};
+  }
+`
