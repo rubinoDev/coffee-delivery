6095453884664e7d2928893b3a7a1592689c0f31
Victor Rubino <rubinodev@gmail.com>
2022-09-06 21:47:56 -0300

    finish success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,58 @@
+import { DeliveryStatsContainer, SuccessContainer } from "./styles";
+import ManOnMotorcycle from '../../assets/Illustration.png'
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+
 export function Success(){
   return(
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h2>Uhu! Pedido confirmado</h2>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+
+      <div>
+        <DeliveryStatsContainer>
+          <div>
+            <div className="mapPin">
+              <MapPin
+                size={20}
+                weight="fill"
+                color="white"
+              />
+            </div>
+            <div>
+              <p>Entrega em <strong>Rua João Daniel Martinelli, 102</strong></p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </div>
+          <div>
+            <div className="timer">
+              <Timer
+                size={20}
+                weight="fill"
+                color="white"
+              />
+            </div>
+            <div>
+              <p>Previsão de entrega</p>
+              <strong>20 min - 30 min </strong>
+            </div>
+          </div>
+          <div>
+            <div className="dollar">
+              <CurrencyDollar
+                size={20}
+                weight="fill"
+                color="white"
+              />
+            </div>
+            <div>
+              <p>Pagamento na entrega</p>
+              <strong>Cartão de Crédito</strong>
+            </div>
+          </div>
+        </DeliveryStatsContainer>
+
+        <img src={ManOnMotorcycle} alt=""/>
+      </div>
+    </SuccessContainer>
   )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+export const SuccessContainer = styled.section`
+  margin-top: 10rem;
+
+  h2{
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 3.2rem;
+    line-height: 130%;
+
+    color: ${props => props.theme.yellowDark};
+
+  }
+
+  >p{
+    font-size: 2rem;
+    line-height: 130%;
+
+    color: ${props => props.theme.baseSubtitle};
+  }
+
+  >div{
+    display: flex;
+    justify-content: space-between;
+  }
+
+`
+
+export const DeliveryStatsContainer = styled.div`
+  width: 52.6rem;
+  margin-top: 5rem;
+  padding: 5rem;
+  
+  background: 
+    linear-gradient(#FAFAFA, #FAFAFA) padding-box,
+    linear-gradient(to right, hsla(44, 71%, 52%, 1), hsla(259, 93%, 63%, 1)) border-box;
+  border-radius: 6px 36px;
+  border: 1px solid transparent;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  >div{
+    display: flex;
+    gap: 2rem;
+
+    .mapPin{
+      padding: 1rem; 
+      background-color: ${props => props.theme.purple};
+      border-radius: 100%;
+
+      display: flex; 
+      justify-content: center; 
+      align-items: center;
+    }
+
+    .timer{
+      padding: 1rem; 
+      background-color: ${props => props.theme.yellow};
+      border-radius: 100%;
+
+      display: flex; 
+      justify-content: center; 
+      align-items: center;
+    }
+
+    .dollar{
+      padding: 1rem;  
+      background-color: ${props => props.theme.yellowDark};
+      border-radius: 100%;
+
+      display: flex; 
+      justify-content: center; 
+      align-items: center;
+    }
+
+    >div p {
+      font-size: 1.6rem;
+      line-height: 130%;
+      color: ${props => props.theme.baseText};
+
+    }
+
+    >div strong{
+      font-weight: 700;
+      font-size: 1.6rem;
+      line-height: 130%;
+
+      color: ${props => props.theme.baseText};
+    }
+  }
+`
