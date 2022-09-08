70ff626be5d73e2f86a53c6e392a94dd03de3093
Victor Rubino <rubinodev@gmail.com>
2022-09-08 19:48:48 -0300

    feature add product to cart

--- src/contexts/CartContext/CartContext.tsx
+++ src/contexts/CartContext/CartContext.tsx
@@ -16,9 +16,20 @@ export function CartContextProvider({children}: CartContextProviderProps){
   const { products } = useContext(ProductsContext)
   const [cart, setCart] = useState<Coffee[]>([])
 
+
   function handleAddCoffeeToCart(id: number){
+    const updatedCart = [...cart];
+    const isCartEmpty = updatedCart.length === 0;
     const productFiltered = products.filter( product => product.id === id);
-    setCart(prevState => [...prevState, productFiltered[0]])
+    const productIsAlreadyInCart = updatedCart.find(product => product.id === productFiltered[0].id);
+
+    isCartEmpty ? setCart(productFiltered) 
+    : productIsAlreadyInCart ? setCart(prevState => 
+        prevState.map(item =>{
+          const quantityUpdated = item.quantity + productFiltered[0].quantity;
+          return {...item, quantity: quantityUpdated}
+    }))
+    : setCart(prevState => [...prevState, productFiltered[0]])    
   }
 
 
